b61ad0da29dc756831971a7cf3588c263dee603e
Darlisson Limeira <ps71036@gmail.com>
2022-07-13 22:39:51 -0300

    update styles

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,3 +8,13 @@ form.addEventListener("submit", (event) => {
 
   return;
 });
+
+{
+  /* <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
+      rel="preload"
+      as="style"
+    /> */
+}
